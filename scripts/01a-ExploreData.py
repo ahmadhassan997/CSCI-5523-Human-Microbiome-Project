5dc06af6e23a83d04e02ccc900b4a675606fc7f2
ahmadhassan997 <ahmadhassansandhu@gmail.com>
2021-03-30 14:27:25 -0500

    feat: Summary statistics for stool data

--- scripts/01a-ExploreData.py
+++ scripts/01a-ExploreData.py
@@ -7,12 +7,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    abundance_df = pd.read_csv(path.join(context.proj_dir, 'data', 'abundance.csv'))
     abundance_stool_df = pd.read_csv(path.join(context.proj_dir, 'data', 'abundance_stoolsubset.csv'))
-    # marker_presence_df = pd.read_csv(path.join(context.proj_dir, 'data', 'marker_presence.csv'))
-    markers2clades_df = pd.read_csv(path.join(context.proj_dir, 'data', 'markers2clades_DB.csv'))
+    summary = abundance_stool_df.mask(abundance_stool_df == 0).describe()
+    summary.reset_index(inplace=True)
+    summary = summary.transpose()
+    summary.reset_index(inplace=True)
+    summary.columns = summary.iloc[0]
+    summary.drop([0], inplace=True)
+    summary.sort_values(by='count', inplace=True, ascending=False)
+    summary.to_csv(path.join(context.proj_dir, 'data_processed', 'summary', 'stool_bacteria_summary.csv'),
+                   index=False)
+    print('Bacteria Count > 1000', summary[summary['count'] > 1000].shape[0])
+    summary = abundance_stool_df.mask(abundance_stool_df == 'nd').describe(include='object')
+    summary.reset_index(inplace=True)
+    summary = summary.transpose()
+    summary.reset_index(inplace=True)
+    summary.columns = summary.iloc[0]
+    summary.drop([0], inplace=True)
+    summary.sort_values(by='count', inplace=True, ascending=False)
+    summary.to_csv(path.join(context.proj_dir, 'data_processed', 'summary', 'stool_object_summary.csv'),
+                   index=False)
 
-    print(abundance_df.describe())
-    print(abundance_stool_df.describe())
-    # print(marker_presence_df.describe())
-    print(markers2clades_df.describe())
